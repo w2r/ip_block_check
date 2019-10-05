55f770fdd4affa5f3660ee8d24637933453e212e
cherubim <wdl1055258582@gmail.com>
2019-10-06 00:49:17 +0800

    update

--- ip_block_check.py
+++ ip_block_check.py
@@ -15,8 +15,19 @@ def run():
     info = info1.json()
     update_id = 184912313
 
-    # 欢迎语
-    text = "请务必阅读关于后输出正确指令，否则可能报错"
+    # 温馨提示欢迎语
+    text0 = "请务必阅读关于后输出正确指令，否则可能报错" + "\n"
+    text1 = "%23+%e5%9f%9f%e5%90%8d%e6%88%96%e8%80%85ip%e6%a0%bc%e5%bc%8f%e5%bf%85%e9%a1%bb%e6%98%afIP%2fPort" + "\n"
+    text2 = "%23+%e9%bb%98%e8%ae%a4%e6%9c%ba%e5%99%a8%e4%ba%ba%e4%bc%9a%e6%af%8f%e5%a4%a912%3a00%3a00%e6%8e%a8%e9%80%81%e5%b7%b2%e4%bf%9d%e5%ad%98ip%e7%9a%84%e6%9f%a5%e8%af%a2%e7%bb%93%e6%9e%9c" + "\n"
+    text3 = "%23+%e6%9f%a5%e8%af%a2%e5%9f%9f%e5%90%8dwww.google.com" + "\n"
+    text4 = "%26ip_check+www.google.com%2f80" + "\n"
+    text5 = "%23+%e6%9f%a5%e8%af%a2140.238.33.201%2f22" + "\n"
+    text6 = "%26ip_check+140.238.33.201%2f22" + "\n"
+    text7 = "%23+%e6%9f%a5%e8%af%a2%e5%b7%b2%e4%bf%9d%e5%ad%98%e5%9f%9f%e5%90%8d" + "\n"
+    text8 = "%26ip_saved" + "\n"
+    text9 = "%23+%e5%88%a0%e9%99%a4%e5%9f%9f%e5%90%8d%e8%ae%b0%e5%bd%95140.238.33.201%2f22" + "\n"
+    text10 = "%26ip_delete+140.238.33.201%2f22"
+    text = url + text0 + text1 + text2 + text3 + text4 + text5 + text6 + text7 + text8 + text9 + text10
     post2tg.post(info["result"][-1]["message"]["from"]["id"], text)
 
     # 主程序
