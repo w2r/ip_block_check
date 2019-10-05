333d936d97ebcc44388b05d014b5e6bb2c707d87
cherubim <wdl1055258582@gmail.com>
2019-10-06 04:23:24 +0800

    update

--- ip_block_check.py
+++ ip_block_check.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+
 import requests
 import time
 import db
@@ -9,13 +10,13 @@ import post2tg
 def run():
 
     # 从tg机器人获得信息
-    html = f"https://api.telegram.org/bot981790366:AAHPBpLzVZXzvRiAV4jx7HHJ3**********/getUpdates"
+    html = f"https://api.telegram.org/bot981790366:AAHPBpLzVZXzvRiAV4jx7HHJ3ZAdSVRB0SI/getUpdates"
     info1 = requests.get(html)
     info1.encoding = 'utf-8'
     info = info1.json()
     update_id = 184912313
 
-    # 温馨提示欢迎语
+    # 温馨欢迎语
     text0 = "请务必阅读关于后输出正确指令，否则可能报错" + "\n"
     text1 = "%23+%e5%9f%9f%e5%90%8d%e6%88%96%e8%80%85ip%e6%a0%bc%e5%bc%8f%e5%bf%85%e9%a1%bb%e6%98%afIP%2fPort" + "\n"
     text2 = "%23+%e9%bb%98%e8%ae%a4%e6%9c%ba%e5%99%a8%e4%ba%ba%e4%bc%9a%e6%af%8f%e5%a4%a912%3a00%3a00%e6%8e%a8%e9%80%81%e5%b7%b2%e4%bf%9d%e5%ad%98ip%e7%9a%84%e6%9f%a5%e8%af%a2%e7%bb%93%e6%9e%9c" + "\n"
@@ -60,7 +61,7 @@ def run():
             update_id = info["result"][-1]["update_id"]
 
             # 查看保存的ip/port
-            if "&ip_saved" in info["result"][-1]["message"]["text"]:
+            if ("&ip_saved" or "/ip_save") in info["result"][-1]["message"]["text"]:
                 text = "你保存的ip记录为："
                 post2tg.post(info["result"][-1]["message"]["from"]["id"], text)
                 for x in db.IP_Port.find({"username": info["result"][-1]["message"]["from"]["username"]}):
@@ -76,6 +77,23 @@ def run():
                     y_delete = {"ip_port": "-1"}
                     # 删除数据库保存的ip(将其值重置为-1)
                     db.IP_Port.update_one(y, {"$set": y_delete})
+
+            # 快捷指令
+            # 快速查询已保存ip状态
+            if "/ip_check " in info["result"][-1]["message"]["text"]:
+                for x in db.IP_Port.find({"chat_id": info["result"][-1]["message"]["from"]["id"]}):
+                    try:
+                        result.check_result(x['chat_id'], x['ip_port'])
+                    except IndexError:
+                        pass
+
+            # 快速删除所有保存ip
+            if "/ip_delete" in info["result"][-1]["message"]["text"]:
+                for y in db.IP_Port.find({"chat_id": info["result"][-1]["message"]["from"]["id"]}):
+                    y_delete = {"ip_port": "-1"}
+                    db.IP_Port.update_one(y, {"$set": y_delete})
+                post2tg.post(info["result"][-1]["message"]["from"]["id"], "已删除服务器保存IP/Port")
+
         time.sleep(3)
 
 
