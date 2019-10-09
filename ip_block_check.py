d097629867baeb956e8dadf57ddeec74041edda9
cherubim <wdl1055258582@gmail.com>
2019-10-10 05:57:36 +0800

    update

--- ip_block_check.py
+++ ip_block_check.py
@@ -9,7 +9,7 @@ import post2tg
 
 def run():
     # 从tg机器人获得信息
-    html = f"https://api.telegram.org/bot981790366:AAHPBpLzVZXzvRiAV4jx7HHJ3ZAdSVRB0SI/getUpdates"
+    html = f"https://api.telegram.org/bot983128077:AAHYi5ouhw_pMnQe9ZwcANSKpsT3zs14Mn4/getUpdates"
     info1 = requests.get(html)
     info1.encoding = 'utf-8'
     info = info1.json()
@@ -19,8 +19,8 @@ def run():
     # 解决因tg编译已发送信息造成错误
     try:
         text0 = "请务必阅读关于后输出正确指令，否则可能报错" + "\n"
-        text1 = "%23+%e5%9f%9f%e5%90%8d%e6%88%96%e8%80%85ip%e6%a0%bc%e5%bc%8f%e5%bf%85%e9%a1%bb%e6%98%afIP%2fPort" + "\n"
-        text2 = "%23+%e9%bb%98%e8%ae%a4%e6%9c%ba%e5%99%a8%e4%ba%ba%e4%bc%9a%e6%af%8f%e5%a4%a912%3a00%3a00%e6%8e%a8%e9%80%81%e5%b7%b2%e4%bf%9d%e5%ad%98ip%e7%9a%84%e6%9f%a5%e8%af%a2%e7%bb%93%e6%9e%9c" + "\n"
+        # text1 = "%23+%e5%9f%9f%e5%90%8d%e6%88%96%e8%80%85ip%e6%a0%bc%e5%bc%8f%e5%bf%85%e9%a1%bb%e6%98%afIP%2fPort" + "\n"
+        # text2 = "%23+%e9%bb%98%e8%ae%a4%e6%9c%ba%e5%99%a8%e4%ba%ba%e4%bc%9a%e6%af%8f%e5%a4%a912%3a00%3a00%e6%8e%a8%e9%80%81%e5%b7%b2%e4%bf%9d%e5%ad%98ip%e7%9a%84%e6%9f%a5%e8%af%a2%e7%bb%93%e6%9e%9c" + "\n"
         text3 = "%23+%e6%9f%a5%e8%af%a2%e5%9f%9f%e5%90%8dwww.google.com" + "\n"
         text4 = "%26ip_check+www.google.com%2f80" + "\n"
         text5 = "%23+%e6%9f%a5%e8%af%a2140.238.33.201%2f22" + "\n"
@@ -28,8 +28,10 @@ def run():
         text7 = "%23+%e6%9f%a5%e8%af%a2%e5%b7%b2%e4%bf%9d%e5%ad%98%e5%9f%9f%e5%90%8d" + "\n"
         text8 = "%26ip_saved" + "\n"
         text9 = "%23+%e5%88%a0%e9%99%a4%e5%9f%9f%e5%90%8d%e8%ae%b0%e5%bd%95140.238.33.201%2f22" + "\n"
-        text10 = "%26ip_delete+140.238.33.201%2f22"
-        text = text0 + text1 + text2 + text3 + text4 + text5 + text6 + text7 + text8 + text9 + text10
+        text10 = "%26ip_delete+140.238.33.201%2f22" + "\n"
+        text11 = '%23 %e6%9f%a5%e8%af%a2%e6%89%80%e6%9c%89%e5%b7%b2%e4%bf%9d%e5%ad%98%e7%9a%84%e5%9f%9f%e5%90%8d' + "\n"
+        text12 = "%2fip_check"
+        text = text0 + text3 + text4 + text5 + text6 + text7 + text8 + text9 + text10 + text11 + text12
         post2tg.post(info["result"][-1]["message"]["from"]["id"], text)
     except KeyError:
         pass
@@ -78,10 +80,8 @@ def run():
 
                 # 快速查询已保存ip状态
                 if "/ip_check" in info["result"][-1]["message"]["text"]:
-                    print(info["result"][-1]["message"]["text"])
                     for x in db.IP_Port.find({"chat_id": info["result"][-1]["message"]["from"]["id"]}):
                         try:
-                            print(x)
                             result.check_result(x['chat_id'], x['ip_port'])
                         except IndexError:
                             pass
@@ -104,15 +104,7 @@ def run():
             except KeyError:
                 pass
 
-        time.sleep(3)
-    while True:
-        # 输出数据库中结果(每天十二点推送）
-        if time.strftime("%T") == "12:00:00":
-            for x in db.IP_Port.find():
-                try:
-                    result.check_result(x['chat_id'], x['ip_port'])
-                except IndexError:
-                    pass
+        time.sleep(5)
 
 
 run()
--- post2tg.py
+++ post2tg.py
@@ -4,7 +4,7 @@ import requests
 
 
 def post(chat_id, text):
-    post_url = 'https://api.telegram.org/bot981790366:AAHPBpLzVZXzvRiAV4jx7HHJ3ZAdSVRB0SI/sendMessag' \
+    post_url = 'https://api.telegram.org/bot983128077:AAHYi5ouhw_pMnQe9ZwcANSKpsT3zs14Mn4/sendMessag' \
                'e?chat_id={0}&text={1}'.format(chat_id, text)
     headers = {
         'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36'}
